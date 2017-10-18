3c64ff20d537cdda9e9b3ad01077bc67f4153daf
Desio05 <gcinbax@gmail.com>
2017-10-19 00:31:50 +0300

    comments

--- src/main/java/ru/desiolab/bnb/algorithms/BranchAndBoundsMaxClique.java
+++ src/main/java/ru/desiolab/bnb/algorithms/BranchAndBoundsMaxClique.java
@@ -7,13 +7,11 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class BranchAndBoundsMaxClique {
 
     private final Graph graph;
     private List<Node> cliqueMax;
-    private int skips = 0;
 
     public BranchAndBoundsMaxClique(Graph graph) {
         this.graph = graph;
@@ -22,19 +20,15 @@ public class BranchAndBoundsMaxClique {
     public List<Node> findClique(Map<Integer, Integer> colors) {
         List<Node> nodes = graph.getNodes();
         nodes.sort(Comparator.<Node>comparingInt(value -> colors.get(value.getIndex())).reversed());
-        System.out.println(nodes.stream().map(Node::getIndex).collect(Collectors.toList()));
 
         cliqueMax = new ArrayList<>();
         branchAndBounds(nodes, new ArrayList<>(), colors);
-        System.out.println("Skips: " + skips);
         return cliqueMax;
     }
 
     private void branchAndBounds(List<Node> candidates, List<Node> clique, final Map<Integer, Integer> colors) {
-//        System.out.println(clique.stream().map(Node::getIndex).collect(Collectors.toList()));
         for (Node candidate : candidates) {
-            if (clique.size() + colors.get(candidate.getIndex()) <= cliqueMax.size()) {
-                skips++;
+            if (clique.size() + colors.get(candidate.getIndex()) <= cliqueMax.size()) { // |Q| + max{Color(candidate)} <= |Qmax|
                 return;
             }
             List<Node> currentClique = new ArrayList<>(clique);
@@ -43,11 +37,11 @@ public class BranchAndBoundsMaxClique {
             List<Node> currentCandidates = new ArrayList<>(candidates);
             int trimIndex = currentCandidates.indexOf(candidate); //exclude edge repeatness
             currentCandidates = currentCandidates.subList(trimIndex, currentCandidates.size());
-            currentCandidates.retainAll(candidate.getNeighbours());
+            currentCandidates.retainAll(candidate.getNeighbours()); // clean up candidates (saving order)
 
             if (currentCandidates.size() > 0) {
-                Map<Integer, Integer> newColors = GreedyGraphColoringAlgorithm.forNodes(currentCandidates);
-                currentCandidates.sort(Comparator.<Node>comparingInt(value -> newColors.get(value.getIndex())).reversed());
+                Map<Integer, Integer> newColors = GreedyGraphColoringAlgorithm.forNodes(currentCandidates); //recalculate subgraph colors
+                currentCandidates.sort(Comparator.<Node>comparingInt(value -> newColors.get(value.getIndex())).reversed()); //sort by color numbers (descending)
                 branchAndBounds(currentCandidates, currentClique, newColors);
             } else if (cliqueMax.size() < currentClique.size()) {
                 cliqueMax = currentClique;
