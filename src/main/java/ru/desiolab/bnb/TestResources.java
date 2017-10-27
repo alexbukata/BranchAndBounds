ff76e8bb517b77917cc8f470fdc8c3cf318652d8
Desio05 <gcinbax@gmail.com>
2017-10-28 00:30:38 +0300

    sort files by size

--- src/main/java/ru/desiolab/bnb/TestResources.java
+++ src/main/java/ru/desiolab/bnb/TestResources.java
@@ -6,19 +6,23 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TestResources {
     public static void main(String[] args) throws IOException {
         File resourcesDir = new File("resources");
-        BufferedWriter writer = new BufferedWriter(new FileWriter("resources/output.txt"));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
         Application.GraphJob graphJob = new Application.GraphJob();
-        for (String testFileName : resourcesDir.list()) {
+        List<File> files = Arrays.stream(resourcesDir.listFiles()).sorted(Comparator.comparingLong(File::length)).collect(Collectors.toList());
+        for (File testFile : files) {
             long first = System.currentTimeMillis();
-            List<Node> nodes = graphJob.graphJob("resources/" + testFileName);
+            List<Node> nodes = graphJob.graphJob(testFile.getPath());
             float result = (System.currentTimeMillis() - first) / 1000f;
             System.out.println("done");
-            writer.write(testFileName + " " + nodes.size() + " " + result);
+            writer.write(testFile.getName() + " " + nodes.size() + " " + result);
             writer.write("\n");
             writer.flush();
         }
