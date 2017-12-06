cdcc776c7b4651b110879f9d66c2a63d6d72cd62
Alexander Bukata <gcinbax@gmail.com>
2017-12-06 15:20:07 +0300

    work in progress

--- src/main/java/ru/desiolab/bnb/algorithms/BranchAndBoundsMaxClique.java
+++ src/main/java/ru/desiolab/bnb/algorithms/BranchAndBoundsMaxClique.java
@@ -40,7 +40,9 @@ public class BranchAndBoundsMaxClique {
             currentCandidates.retainAll(candidate.getNeighbours()); // clean up candidates (saving order)
 
             if (currentCandidates.size() > 0) {
-                Map<Integer, Integer> newColors = GreedyGraphColoringAlgorithm.calculate(currentCandidates); //recalculate subgraph colors
+                GreedyGraphColoringAlgorithm coloringAlgorithm = new GreedyGraphColoringAlgorithm(currentCandidates);
+                coloringAlgorithm.calculate();
+                Map<Integer, Integer> newColors = coloringAlgorithm.getColors(); //recalculate subgraph colors
                 currentCandidates.sort(Comparator.<Node>comparingInt(value -> newColors.get(value.getIndex())).reversed()); //sort by color numbers (descending)
                 branchAndBounds(currentCandidates, currentClique, newColors);
             } else if (cliqueMax.size() < currentClique.size()) {
