f7bb00cd19b28b36c8b7b7f20ba73f0583b8a3e9
Desio05 <gcinbax@gmail.com>
2017-12-08 01:18:14 +0300

    wip

--- src/main/java/ru/desiolab/bnb/algorithms/BranchAndBoundsCplex.java
+++ src/main/java/ru/desiolab/bnb/algorithms/BranchAndBoundsCplex.java
@@ -1,15 +1,20 @@
 package ru.desiolab.bnb.algorithms;
 
 import ilog.concert.IloException;
+import ilog.concert.IloLinearIntExpr;
 import ilog.concert.IloLinearNumExpr;
 import ilog.concert.IloNumVar;
 import ilog.cplex.IloCplex;
 import ru.desiolab.bnb.graph.Node;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class BranchAndBoundsCplex {
+    private final static double EPSILON = 0.01;
+
     private final List<Node> nodes;
     private final List<Set<Node>> independentSets;
     private final Integer chromaticNumber;
@@ -22,33 +27,61 @@ public class BranchAndBoundsCplex {
         this.chromaticNumber = chromaticNumber;
     }
 
+    private IloCplex initSolver() throws IloException {
+        IloCplex cplex = new IloCplex();
+        IloNumVar[] x = new IloNumVar[nodes.size() + 1];
+        for (int i = 1; i <= nodes.size(); i++) {
+            x[i] = cplex.numVar(0, 1);
+        }
+        IloLinearNumExpr objective = cplex.linearNumExpr();
+        for (int i = 1; i <= nodes.size(); i++) {
+            objective.addTerm(1, x[i]);
+        }
+        cplex.addMaximize(objective);
+
+        BranchAndBoundConstraints.eachNodeConstraint(cplex, nodes, x);
+        BranchAndBoundConstraints.roundRobinConstraints(cplex, nodes, x);
+        BranchAndBoundConstraints.independentSetsConstraints(cplex, independentSets, x);
+
+        return cplex;
+    }
+
     public void compute() {
         try {
-            IloCplex cplex = new IloCplex();
-            IloNumVar[] x = new IloNumVar[nodes.size() + 1];
-            for (int i = 1; i <= nodes.size(); i++) {
-                x[i] = cplex.numVar(0, 1);
-            }
-            IloLinearNumExpr objective = cplex.linearNumExpr();
-            for (int i = 1; i <= nodes.size(); i++) {
-                objective.addTerm(1, x[i]);
-            }
-            cplex.addMaximize(objective);
-
-            BranchAndBoundConstraints.eachNodeConstraint(cplex, nodes, x);
-            BranchAndBoundConstraints.roundRobinConstraints(cplex, nodes, x);
-            BranchAndBoundConstraints.independentSetsConstraints(cplex, independentSets, x);
-
+            IloCplex cplex = initSolver();
             cplex.solve();
-
-            double objValue = cplex.getObjValue();
-            System.out.println(objValue);
-            System.out.println();
-            for (int i = 1; i < x.length; i++) {
-                System.out.println(i + ": " + cplex.getValue(x[i]));
-            }
+            Map<Integer, Integer> additionalNodesConstraints = new HashMap<>();
+            branching(cplex, additionalNodesConstraints);
         } catch (IloException e) {
             e.printStackTrace();
         }
+    }
+
+    private void branching(IloCplex previousSolver, Map<Integer, Integer> additionalNodesConstraints) throws IloException {
+        Integer branchCandidate = -1;
+        for (int i = 1; i < x.length; i++) {
+            double value = previousSolver.getValue(x[i]);
+            if (value < 1.0 - EPSILON || value > 0.0 + EPSILON) {
+                branchCandidate = i;
+                break;
+            }
+        }
+        if (branchCandidate == -1) {
+            System.out.println("Clique has been found!");
+            return;
+        }
+        IloCplex branch1 = initSolver();
+        HashMap<Integer, Integer> firstBranchConstraints = new HashMap<>(additionalNodesConstraints);
+        firstBranchConstraints.put(branchCandidate, 0);
+        for (Map.Entry<Integer, Integer> constraint : firstBranchConstraints.entrySet()) {
+            IloLinearIntExpr linearIntExpr = branch1.linearIntExpr();
+            linearIntExpr.addTerm(x[constraint.getKey()]);
+            if (constraint.getValue() == 0) {
+            }
+        }
+
+        IloCplex branch2 = initSolver();
+        HashMap<Integer, Integer> secondBranchConstraints = new HashMap<>(additionalNodesConstraints);
+        secondBranchConstraints.put(branchCandidate, 1);
     }
 }
